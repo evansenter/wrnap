7b0034bc4b9b6a25c50e1f05477cd1f0e9e286eb
Evan Senter <evansenter@gmail.com>
2014-04-14 12:24:13 -0400

    Updating FftMfpt package to use the multi_param syntax

--- lib/wrnap/package/fft_mfpt.rb
+++ lib/wrnap/package/fft_mfpt.rb
@@ -5,17 +5,25 @@ module Wrnap
   module Package
     class FftMfpt < Base
       self.executable_name = "FFTmfpt"
-      
-      attr_reader :mfpt
-    
-      def run_command(flags)      
-        "%s %s %s" % [
-          exec_name, 
-          stringify_flags(flags), 
-          data.temp_fa_file!
-        ]
+
+      self.default_flags = ->(context, flags) do
+        {
+          "-fftbor2d-i" => context.data.seq,
+          "-fftbor2d-j" => context.data.str_1,
+          "-fftbor2d-k" => context.data.str_2,
+          "-mfpt-x"     => true
+        }
       end
-    
+      self.quote_flag_params = %w|-fftbor2d-i -fftbor2d-j -fftbor2d-k|
+
+      attr_reader :mfpt
+
+      def run_command(flags)
+        Wrnap.debugger { "Running #{exec_name} on #{data.inspect}" }
+
+        "%s %s" % [exec_name, stringify_flags(flags)]
+      end
+
       def post_process
         @mfpt = response.to_f
       end
