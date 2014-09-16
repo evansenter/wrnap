31ff7739f87c26d489f3fdb6fbac6e655ed9316a
Evan Senter <evansenter@gmail.com>
2014-09-16 12:24:09 -0400

    Entrez calls output to console

--- lib/wrnap/global/entrez.rb
+++ lib/wrnap/global/entrez.rb
@@ -19,16 +19,18 @@ module Wrnap
         def sequence_from_entrez(id, position, window)
           Wrnap.debugger { "Retrieving sequence from Entrez: using nuccore DB (id: #{id}, seq_start: #{position + window.min}, seq_stop: #{position + window.max})" }
           Wrnap.debugger { "> True starting position: #{position} with window #{window.min} to #{window.max}" }
-
+          
           fasta = ::Entrez.EFetch("nuccore", {
             id:        id,
             seq_start: position + window.min,
             seq_stop:  position + window.max,
             retmode:   :fasta,
             rettype:   :text
-          }).response.body
+          })
+          
+          Wrnap.debugger { fasta }
 
-          Bio::FastaFormat.new(fasta)
+          Bio::FastaFormat.new(fasta.response.body)
         end
       end
     end
