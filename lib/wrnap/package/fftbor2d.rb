9c9a69f0b9d4b61dea7e69eda12f31bcba131fac
Evan Senter <evansenter@gmail.com>
2014-11-06 08:59:50 -0500

    Better RNA .fa parsing
    Signed-off-by: Evan Senter <evansenter@gmail.com>

--- lib/wrnap/package/fftbor2d.rb
+++ lib/wrnap/package/fftbor2d.rb
@@ -2,12 +2,12 @@ module Wrnap
   module Package
     class Fftbor2d < EnergyGrid2d
       self.executable_name = "FFTbor2D"
-      self.default_flags   = ->(_, flags) { (flags.keys & %i|M S|).empty? ? { S: :empty } : {} }
+      self.default_flags   = ->(_, flags) { (flags.keys & %i|m s|).empty? ? { s: :empty } : {} }
 
       def run_command(flags)
         Wrnap.debugger { "Running #{exec_name} on #{data.inspect}" }
 
-        "%s %s %s" % [
+        "%s %s -f %s" % [
           exec_name,
           stringify_flags(flags),
           data.temp_fa_file!
@@ -15,7 +15,7 @@ module Wrnap
       end
 
       def distribution
-        response.split(/\n/).map { |line| line.split(/\t/) }
+        response.split(/\n/).map { |line| line.scanf("%d\t%d\t%f\t%f") }
       end
     end
   end
--- lib/wrnap/rna.rb
+++ lib/wrnap/rna.rb
@@ -15,19 +15,24 @@ module Wrnap
     attr_reader   :sequence, :structures, :metadata
 
     class << self
-      def init_from_string(sequence, structures = nil, comment = nil, &block)
-        new(
-          sequence:   sequence,
-          structures: structures,
-          comment:    comment,
-          &block
-        )
+      def init_from_string(sequence, *remaining_args, &block)
+        init_from_hash(parse_rna_attributes(sequence, remaining_args), &block)
+      end
+      
+      def parse_rna_attributes(sequence, attributes = [])
+        last_arg = (attributes = attributes.flatten).last
+        
+        if last_arg.is_a?(Symbol) || Regexp.compile("^[\\.\\(\\)]{%d}$" % sequence.length) =~ last_arg
+          { seq: sequence, strs: attributes }
+        else
+          { seq: sequence, strs: attributes[0..-2], comment: last_arg }
+        end
       end
 
       def init_from_hash(hash, &block)
         new(
           sequence:   hash[:sequence]   || hash[:seq],
-          structures: hash[:structures] || hash[:structure] || hash[:str],
+          structures: hash[:structures] || hash[:structure] || hash[:strs] || hash[:str],
           comment:    hash[:comment]    || hash[:name],
           &block
         )
@@ -37,7 +42,7 @@ module Wrnap
         init_from_string(*array, &block)
       end
 
-      def init_from_fasta(string, &block)
+      alias_method :init_from_fa, def init_from_fasta(string, &block)
         if File.exist?(string)
           comment = File.basename(string, string.include?(?.) ? ".%s" % string.split(?.)[-1] : "")
           string  = File.read(string).chomp
@@ -45,12 +50,12 @@ module Wrnap
           
         if string.count(?>) > 1
           string.split(/>/).reject(&:empty?).map do |rna_string|
-            rna_data = rna_string.split(?\n)
+            rna_data = rna_string.split(?\n).reject(&:empty?)
             init_from_string(*rna_data[1..-1]).copy_name_from(rna_data[0])
           end.wrnap
         else
-          init_from_string(*string.split(/\n/).reject { |line| line.start_with?(">") }[0, 3], &block).tap do |rna|
-            if (line = string.split(/\n/).first).start_with?(">") && !(file_comment = line.gsub(/^>\s*/, "")).empty?
+          init_from_string(*string.split(?\n).reject { |line| line.start_with?(?>) || line.empty? }, &block).tap do |rna|
+            if (line = string.split(?\n).first).start_with?(?>) && !(file_comment = line.gsub(/^>\s*/, "")).empty?
               rna.comment = file_comment
             elsif comment
               rna.comment = comment
@@ -82,7 +87,7 @@ module Wrnap
       @metadata   = Metadata::Container.new(self)      
       @structures = (structures ? [structures].flatten : []).each_with_index.map do |structure, i|
         case structure
-        when :empty then empty_structure
+        when :empty, :empty_str then empty_structure
         when :mfe   then RNA(@sequence).run(:fold).mfe_rna.structure
         when String then structure
         when Hash   then
